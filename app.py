2d463f09779c4269900618702af944ec4718a099
rafueru <rafael.albuquerque@gmail.com>
2023-10-23 20:28:09 -0300

    Swagger documentation

--- app.py
+++ app.py
@@ -1,29 +1,51 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, g
 import sqlite3
+from flasgger import Swagger
+
+DATABASE = 'database.db'
 
 app = Flask(__name__)
+swagger = Swagger(app)
 
-# Inicializar o banco de dados
-def init_db():
-    with sqlite3.connect('database.db') as conn:
-        conn.execute('''CREATE TABLE IF NOT EXISTS developers
-                         (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, city TEXT, state TEXT, phone TEXT, email TEXT, experience TEXT, skills TEXT, linkedIn TEXT, employment_status TEXT, salary_expectation REAL, other_info TEXT, resume BLOB)''')
-        conn.execute('''CREATE TABLE IF NOT EXISTS recruiters
-                         (id INTEGER PRIMARY KEY, name TEXT, filter_city TEXT, filter_state TEXT, filter_salary REAL, filter_area TEXT, contact TEXT, status TEXT)''')
-    print("Database initialized!")
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
 
-init_db()
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
-@app.route('/submit_developer', methods=['POST'])
-def submit_developer():
-    # Aqui, você capturaria os dados do formulário e os salvaria no banco de dados.
-    # Por simplicidade, estou apenas retornando uma mensagem.
-    return jsonify({"message": "Developer data submitted!"})
-
-@app.route('/submit_recruiter', methods=['POST'])
-def submit_recruiter():
-    # Similar ao anterior, mas para os recrutadores.
-    return jsonify({"message": "Recruiter data submitted!"})
+@app.route('/submit', methods=['POST'])
+def submit_data():
+    """
+    Submit Data API
+    ---
+    parameters:
+      - name: body
+        in: body
+        required: true
+        schema:
+          type: object
+          properties:
+            name:
+              type: string
+    responses:
+      200:
+        description: Data received
+        schema:
+          type: object
+          properties:
+            message:
+              type: string
+            data:
+              type: object
+    """
+    data = request.json
+    return jsonify({"message": "Data received", "data": data}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
